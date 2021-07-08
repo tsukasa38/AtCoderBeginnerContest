df20ddbb9ad3b9c873936b7c706827a2b71dbb96
tsukasa38 <104.tsukasa@gmail.com>
2021-07-08 17:05:40 +0000

    AC ABC199-C

--- src/ABC199/C.cpp
+++ src/ABC199/C.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-void str_swap(string &str, int i, int j) {
+void str_swap(string &str, int &i, int &j) {
     int tmp = str.at(i);
     str.at(i) = str.at(j);
     str.at(j) = tmp;
@@ -14,7 +14,9 @@ void str_n_swap(string &str) {
     int n = str.size() / 2;
 
     for(int i = 0; i < n; ++i) {
-        str_swap(str, i, n+i);
+        int index1 = i;
+        int index2 = n+i;
+        str_swap(str, index1, index2);
     }
 
     return;
@@ -34,6 +36,30 @@ int main() {
         queries.at(i) = make_tuple(t, a, b);
     }
 
+    bool is_n_chars_swap = false;
+
+    for(int i = 0; i < Q; ++i) {
+        int t, a, b;
+        tie(t, a, b) = queries.at(i);
+
+        if(t == 1 && !is_n_chars_swap) {
+            int index1 = a-1;
+            int index2 = b-1;
+            str_swap(S, index1, index2);
+        }
+        if(t == 1 && is_n_chars_swap) {
+            int index1 = (a-1 < N ? a+N-1 : a-N-1);
+            int index2 = (b-1 < N ? b+N-1 : b-N-1);
+            str_swap(S, index1, index2);
+        }
+        if(t == 2) {
+            is_n_chars_swap = !is_n_chars_swap;
+        }
+    }
+
+    if(is_n_chars_swap) { str_n_swap(S); }
+
+    /*
     // TLE O(N*N)
     for(int i = 0; i < Q; ++i) {
         int t, a, b;
@@ -42,6 +68,7 @@ int main() {
         if(t == 1) { str_swap(S, a-1, b-1); }
         if(t == 2) { str_n_swap(S); }
     }
+    */
 
     cout << S << endl;
 
