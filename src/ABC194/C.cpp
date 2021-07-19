efba07ee24e08d492bd50db385ae350f1cc3ffac
tsukasa38 <104.tsukasa@gmail.com>
2021-07-19 12:52:20 +0000

    Save ABC194-C

--- src/ABC194/C.cpp
+++ src/ABC194/C.cpp
@@ -12,17 +12,25 @@ int main() {
         cin >> A.at(i);
     }
 
-    int sum = 0;
+    vector<long long> cumulative_sum(N);
+    vector<long long> cumulative_square_sum(N);
+
+    long long sum1 = 0;
+    long long sum2 = 0;
 
     for(int i = 0; i < N; ++i) {
-        sum += A.at(i);
+        sum1 += A.at(i);
+        sum2 += A.at(i)*A.at(i);
+        cumulative_sum.at(i) = sum1;
+        cumulative_square_sum.at(i) = sum2;
     }
 
-    int squared_error = 0;
+    long long squared_error = 0;
 
-    for(int i = 0; i < N; ++i) {
-        squared_error += (N-1)*A.at(i)*A.at(i);
-        squared_error -= A.at(i)*(sum - A.at(i));
+    for(int i = 1; i < N; ++i) {
+        squared_error += i * A.at(i) * A.at(i);
+        squared_error += cumulative_square_sum.at(i-1);
+        squared_error -= 2 * A.at(i) * cumulative_sum.at(i-1);
     }
 
     cout << squared_error << endl;
